21e69113a78b1a300b9d27105300354f10603ca2
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-16 20:40:02 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,3 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-
-# def parrot
-#   puts "Squawk!"
-#   if parrot("Pretty bird!")
-#     puts "Pretty bird!"
-# end
-
 def parrot(pretty_bird = "Squawk!")
   puts pretty_bird
   return pretty_bird
